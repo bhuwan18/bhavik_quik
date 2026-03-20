d1d430627058b10f6a914a2fb50e0c20d2b55f10
Bhuwan <bhuwan18@gmail.com>
2026-03-20 08:14:37 +0530

    refactor: remove unnecessary urlBase64ToUint8Array function and simplify applicationServerKey assignment

--- components/layout/PushSubscriptionManager.tsx
+++ components/layout/PushSubscriptionManager.tsx
@@ -6,15 +6,6 @@ import { useSession } from "next-auth/react";
 const VAPID_PUBLIC_KEY = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY ?? "";
 const DISMISSED_KEY = "bq_push_dismissed";
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
-  const rawData = atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; i++) outputArray[i] = rawData.charCodeAt(i);
-  return outputArray;
-}
-
 async function subscribeAndSave(): Promise<void> {
   try {
     const reg = await navigator.serviceWorker.ready;
@@ -23,7 +14,7 @@ async function subscribeAndSave(): Promise<void> {
       existing ??
       (await reg.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+        applicationServerKey: VAPID_PUBLIC_KEY,
       }));
 
     const json = sub.toJSON();
